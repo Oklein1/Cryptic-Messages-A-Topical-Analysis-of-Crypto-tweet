1dfe4e3ce81b0c7696521f8c095c45e5a3eb0ee7
jmfinerty <jmfinerty@gmail.com>
2022-03-31 23:11:45 -0400

    add tfidf summaries to main

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pdb
 from time import time
+from tfidf import tfidf
 from nltk import download, RegexpParser
 from text_processing import get_processed_tweets
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-TESTING = False # trigger a breakpoint every time a new tweet is read
+TESTING = False
 
 DATA_CSV_LOC = 'Bitcoin_tweets.csv'
 
@@ -17,6 +18,9 @@ REMOVE_SW = True
 
 # Number of tweets to process. Set small for testing, set to -1 to do entire dataset.
 MAX_TWEETS = 1000
+
+# Print this many of the most 'important' words from each class' TFIDF results
+LEN_TFIDF_SUMM = 10
 
 
 def nltk_download():
@@ -36,6 +40,10 @@ def main():
     num_neu = 0
     num_neg = 0
 
+    pos_tweet_word_counts = {}
+    neg_tweet_word_counts = {}
+    neu_tweet_word_counts = {}
+
     print("PROCESSING...", end='', flush=True)
 
     t0 = time()
@@ -46,27 +54,55 @@ def main():
 
         if score > 0.05:
             num_pos += 1
+            for word in tokens:
+                if word in pos_tweet_word_counts:
+                    pos_tweet_word_counts[word] += 1
+                else:
+                    pos_tweet_word_counts[word] = 1
         elif score < -0.05:
             num_neg += 1
+            for word in tokens:
+                if word in neg_tweet_word_counts:
+                    neg_tweet_word_counts[word] += 1
+                else:
+                    neg_tweet_word_counts[word] = 1
         else:
             num_neu += 1
+            for word in tokens:
+                if word in neu_tweet_word_counts:
+                    neu_tweet_word_counts[word] += 1
+                else:
+                    neu_tweet_word_counts[word] = 1
 
         # Print a . every 5 seconds
         if time() - t > 5:
             print('.', end='', flush=True)
             t = time()
 
-        if TESTING:
-            pdb.set_trace()
 
-    print("DONE. %ss\n" % round(time() - t0, 2))
+    print("DONE. %ss" % round(time() - t0, 2))
 
+    print()
     print("CLASS COUNTS")
     print("Positive Tweets: %s" % num_pos)
     print("Negative Tweets: %s" % num_neg)
     print("Neutral Tweets: %s" % num_neu)
-
     print()
+
+    if TESTING:
+        pdb.set_trace()
+
+    print("TOP %s WORDS" % LEN_TFIDF_SUMM)
+    print("POSITIVE")
+    for word, score in tfidf(pos_tweet_word_counts, num_pos)[:LEN_TFIDF_SUMM+1]:
+        print('\t%s: %s' % (word, score))
+    print("\nNEGATIVE")
+    for word, score in tfidf(neg_tweet_word_counts, num_pos)[:LEN_TFIDF_SUMM+1]:
+        print('\t%s: %s' % (word, score))
+    print("\nNEUTRAL")
+    for word, score in tfidf(neu_tweet_word_counts, num_pos)[:LEN_TFIDF_SUMM+1]:
+        print('\t%s: %s' % (word, score))
+
 
 if __name__ == '__main__':
     main()

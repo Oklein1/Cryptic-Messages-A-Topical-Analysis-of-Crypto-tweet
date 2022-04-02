6979b72d75fb0f7762d9affd0c8ac117a4b62326
jmfinerty <jmfinerty@gmail.com>
2022-04-02 17:55:15 -0400

    fixing weird generator errors

--- bot_pruning.py
+++ bot_pruning.py
@@ -80,10 +80,9 @@ def train():
 
 
 # Gets a specific line from the CSV. MUCH faster than just iterating through csv until desired line
-def get_csv_lines(csv_loc, lines):
+def get_csv_lines(csv_loc, line):
     with open(csv_loc, encoding='utf8') as f:
-        for line in lines:
-            yield next(itertools.islice(csv.reader(f), line, None))
+        yield next(itertools.islice(csv.reader(f), line, None))
 
 
 def manually_tag_data(csv_loc):
@@ -93,28 +92,29 @@ def manually_tag_data(csv_loc):
 
     num_tagged = 0
 
-    for line in get_csv_lines(DATA_CSV_LOC, sample_lines):
-        text = line[9]
+    for line_num in sample_lines:
+        for line in get_csv_lines(DATA_CSV_LOC, line_num):
+            text = line[9]
 
-        print('\n\nTWEET %s/%s' % (num_tagged, NUM_TO_TAG))
-        print('#'*100)
-        print(text)
-        print('#'*100)
+            print('\n\nTWEET %s/%s' % (num_tagged, NUM_TO_TAG))
+            print('#'*100)
+            print(text)
+            print('#'*100)
 
-        tag = None
-        while tag not in ('0', '1', 'x'):
-            if num_tagged == 0:
-                tag = input('Is this spam? (\'0\' if no, \'1\' if yes, \'x\' to exit): ')
-            else:
-                tag = input('Is this spam?: ')
-            if tag == 'x':
-                return
+            tag = None
+            while tag not in ('0', '1', 'x'):
+                if num_tagged == 0:
+                    tag = input('Is this spam? (\'0\' if no, \'1\' if yes, \'x\' to exit): ')
+                else:
+                    tag = input('Is this spam?: ')
+                if tag == 'x':
+                    return
 
-        tokens = process_tweet(text)
-        for token in tokens:
-            outfile.write(token + ' ')
-        outfile.write(tag + '\n')
-        num_tagged += 1
+            tokens = process_tweet(text)
+            for token in tokens:
+                outfile.write(token + ' ')
+            outfile.write(tag + '\n')
+            num_tagged += 1
 
     outfile.close()
 
